556e045457e1e442cc8802a4c4a97141b2479bf9
Alexander <alykoshin@gmail.com>
2019-02-28 17:04:11 +0300

    trivial changes

--- packages/async/lib/isNodejs.js
+++ packages/async/lib/isNodejs.js
@@ -1,1 +1,10 @@
-module.exports = function isNodejs() { return typeof "process" !== "undefined" && process && process.versions && process.versions.node; }
+//https://stackoverflow.com/questions/4224606/how-to-check-whether-a-script-is-running-under-node-js#comment92529277_35813135
+
+const isNodejs = () => (
+  typeof "process" !== "undefined"
+  && process
+  && process.versions
+  && process.versions.node
+);
+
+module.exports = isNodejs;
