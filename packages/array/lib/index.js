d410e34a7def6711fa7f56e56832a19365ebc0f7
Alexander <alykoshin@gmail.com>
2019-07-09 00:08:15 +0300

    add arrayOne,findMatched,removeAt,removeElement,removeMatched

--- packages/array/lib/index.js
+++ packages/array/lib/index.js
@@ -1,5 +1,8 @@
 'use strict';
 
+const _ = require('lodash');
+
+
 
 function peek(array, offset) {
   if (typeof offset !== 'number') offset = 0;
@@ -8,6 +11,51 @@ function peek(array, offset) {
 }
 
 
+const arrayOne = (array, options={}) => {
+  if (array.length < 1) throw new Error('Element not found');
+  if ( options.strict !== false &&
+    array.length > 1) throw new Error('More than one element found');
+  return array[0];
+};
+
+
+const findMatched = (array, match) => {
+  if (typeof array === 'undefined') array = [];
+  array = Array.isArray(array) ? array : [array];
+  return array.filter(_.matches(match));
+};
+
+
+const removeAt = (array, index) => {
+  if (index > -1) {
+    array.splice(index, 1);
+  }
+  return array;
+};
+
+
+const removeElement = (array, element) => {
+  const index = array.indexOf(element);
+  return removeAt(array, index);
+};
+
+
+const removeMatched = (array, filter) => {
+  const elementsToDelete = findMatched(array, filter);
+  elementsToDelete.forEach( el => _.pull(array, el) );
+  return elementsToDelete.length;
+};
+
+
+
+
 module.exports = {
   peek,
+
+  arrayOne,
+  findMatched,
+
+  removeAt,
+  removeElement,
+  removeMatched,
 };
