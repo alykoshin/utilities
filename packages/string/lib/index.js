137ec0dba984a76d4caeb67ac6a2ee829221c86c
Alexander <alykoshin@gmail.com>
2019-09-12 00:11:26 +0300

    add generateUUID

--- packages/string/lib/index.js
+++ packages/string/lib/index.js
@@ -127,6 +127,17 @@ const joinNonEmpty = (arrayOfStrings, delimiter='.') => {
 };
 
 
+function generateUUID(){
+  var d = new Date().getTime();
+  var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+    var r = (d + Math.random()*16)%16 | 0;
+    d = Math.floor(d/16);
+    return (c=='x' ? r : (r&0x3|0x8)).toString(16);
+  });
+  return uuid;
+};
+
+
 
 module.exports = {
   repeat,
@@ -147,4 +158,6 @@ module.exports = {
 
   addfix,
   joinNonEmpty,
+
+  generateUUID,
 };
